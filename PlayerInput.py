efb4f4ccea12b80867cacd15fb54e69e45f915bb
Closeplanet2 <68482902+Closeplanet2@users.noreply.github.com>
2024-03-31 15:30:20 +0100

    Update PlayerInput.py

--- PlayerInput.py
+++ PlayerInput.py
@@ -1,4 +1,4 @@
-def get_user_input(prompt_text, correct_values=None, default_value=None):
+def string_input(prompt_text, correct_values=None, default_value=None):
     while True:
         user_input = input(prompt_text)
         if correct_values is None: return user_input
@@ -7,10 +7,34 @@ def get_user_input(prompt_text, correct_values=None, default_value=None):
         print("Invalid Input!")
 
 
-def get_user_int_input(prompt_text, correct_values=None, default_value=None):
+def int_input(prompt_text, correct_values=None, default_value=None):
     while True:
         try:
             user_input = int(input(prompt_text))
+            if correct_values is None: return user_input
+            if user_input in correct_values: return user_input
+            if default_value is not None: return default_value
+            print("Invalid Input!")
+        except ValueError:
+            if default_value is not None: return default_value
+            print("Invalid number!")
+
+def float_input(prompt_text, correct_values=None, default_value=None):
+    while True:
+        try:
+            user_input = float(input(prompt_text))
+            if correct_values is None: return user_input
+            if user_input in correct_values: return user_input
+            if default_value is not None: return default_value
+            print("Invalid Input!")
+        except ValueError:
+            if default_value is not None: return default_value
+            print("Invalid number!")
+
+def bool_input(prompt_text, correct_values=None, default_value=None):
+    while True:
+        try:
+            user_input = bool(input(prompt_text))
             if correct_values is None: return user_input
             if user_input in correct_values: return user_input
             if default_value is not None: return default_value
